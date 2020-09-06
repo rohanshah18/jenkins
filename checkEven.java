6c9b5c9534f6ef89a7a607d863890cff08dc7285
Rohan Shah <rxs5532@psu.edu>
2020-09-06 13:12:35 -0400

    fixed version mixture

--- checkEven.java
+++ checkEven.java
@@ -1,10 +1,6 @@
 public class checkEven{
 	public static void main(String[] args){
-<<<<<<< HEAD
 		int test_var = 25;
 		System.out.println("Results for " + test_var + "is: " + helper.isEven(25));
-=======
-		System.out.println(helper.isEven(25));
->>>>>>> 81598b7caa4d278b13fceaeb8c4f90395c5bc86c
 	}
 }
