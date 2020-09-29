5aa10d785cb788b94868e9b1beabf4b468b73d02
Rohan Shah <rxs5532@psu.edu>
2020-09-29 09:42:11 -0400

    added multiple functions

--- helper.java
+++ helper.java
@@ -2,4 +2,21 @@ public class helper{
 	public static boolean isEven(int num){
 		return (num%2==0);
 	}
+
+	public static char[] reverseString(String S) {
+		char[] s = S.toCharArray();
+		int left = 0, right = s.length - 1;
+		while (left < right) {
+			char tmp = s[left];
+			s[left++] = s[right];
+			s[right--] = tmp;
+		}
+		return s;
+	}
+
+	public static void main(String[] args) {
+		int test_var = Integer.valueOf(args[0]);
+		System.out.println(isEven(test_var));
+		System.out.println(reverseString(args[1]));
+	}
 }
